a69ad42d40ba5f5a7b5d3a4577f723d684b1fa48
Gracetoffy <tofiotu@gmail.com>
2025-03-26 16:26:04 +0100

    so its kinda working but the accuracy got worse , i tried using media pipe and the names arent even displaying , it only shwing unknown

--- main.py
+++ main.py
@@ -1,82 +1,130 @@
 import os
 import cv2
 import numpy as np
-import face_recognition
+import mediapipe as mp
+from facenet_pytorch import InceptionResnetV1
 from deepface import DeepFace
+import pickle
+
+# Use webcam to detect face
+
+mp_face_detection = mp.solutions.face_detection
+face_detection = mp_face_detection.FaceDetection( model_selection=0, min_detection_confidence=0.7 )
+cap = cv2.VideoCapture(0)
+
+# Use webcam to detect face and extract face embeddings
+facenet = InceptionResnetV1(pretrained='vggface2').eval()
+
+#COSINE SIMILRITY 
+def cosine_similarity(emb1, emb2):
+    return np.dot(emb1, emb2.T)/(np.linalg.norm(emb1)*np.linalg.norm(emb2))
+
+# to get embedding 
+def get_embedding(face):
+    try:
+        embedding=DeepFace.represent(face, model_name='Facenet',enforce_detection=False)
+        return np.array(embedding[0]['embedding'])
+    except Exception as e:
+        print('Error extracting face embedding:', e)
+        return None
+    
+#save known faces embeddings
+def save_known_faces(known_faces, folder="known_faces.pkl"):
+    with open(folder,"wb") as f:
+        pickle.dump(known_faces, f)
+
+#load known faces embeddings
+def load_faces_from_file(folder="known_faces.pkl"):
+    if not os.path.exists(folder):  
+         print("⚠️ No known faces file found. Creating a new one.")
+         return{}
+  # Return an empty dictionary if the file doesn't exist
+
+    with open(folder, "rb") as f:
+        return pickle.load(f)
+
+        known_faces = pickle.load(f)
+        
 
 
-# Folder containing known faces
-KNOWN_FACES_DIR = "faces"
-test_img = "tests.jpg"
+def load_known_faces(folder="faces"):
+    known_faces = load_faces_from_file()
+    if known_faces:
+        print("Known faces loaded from file")
+        return known_faces
+    face_counts={}
+    known_faces={}
+    for filename in os.listdir(folder):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            name = "-".join(os.path.splitext(filename)[0].split("_")[:-1])
+            img_path = os.path.join(folder, filename)
+            img = cv2.imread(img_path)
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results= face_detection.process(img_rgb)
+            if results.detections:
+                for detection in results.detections:
+                    bboxC= detection.location_data.relative_bounding_box
+                    h,w,_ = img.shape
+                    x,y,w_box,h_box = int(bboxC.xmin*w), int(bboxC.ymin*h), int(bboxC.width*w), int(bboxC.height*h)
+                    face_crop = img[y:y+h_box, x:x+w_box] 
+                    if face_crop.shape[0]>0 and face_crop.shape[1]>0:
+                        try:
+                         embedding = DeepFace.represent(face_crop, model_name='Facenet',enforce_detection=False)[0]['embedding']
+                         embedding = np.array(embedding)
+                         if name in known_faces:
+                            known_faces[name] += embedding
+                            face_counts[name] += 1
+                         else:
+                          known_faces[name] = embedding
+                          face_counts[name] = 1
+                          print(f"Embedding for {name} saved")
+                        except Exception as e:
+                            print('Error extracting face embedding:', e)
+    for name in known_faces.keys():
+        known_faces[name] /= face_counts[name]
+    save_known_faces(known_faces)
+    known_faces = load_known_faces()
+                       
 
-test_face = cv2.imread(test_img)
-if test_face is None:
-    print(f"Error: Could not load image {test_img}")
-    exit(1)
-
-test_rgb = cv2.cvtColor(test_face, cv2.COLOR_BGR2RGB)
-
-#FACE ENCODDING 
-face_encoding = face_recognition.face_encodings(test_rgb)[0]
-
-
-
-
-# Loop through images in the folder
-for filename in os.listdir(KNOWN_FACES_DIR):
-    if filename.lower().endswith((".jpg",".png")) :
-        person_name = os.path.splitext(filename)[0]  # Extract name from filename
-        img_path = os.path.join(KNOWN_FACES_DIR, filename)
-        img = cv2.imread(img_path)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        sample_encoding = face_recognition.face_encodings(img_rgb)[0]
-
-        result =face_recognition.compare_faces([face_encoding], sample_encoding, tolerance=0.6)
-        print(result)
-            
-            
-
-
-"""          print(f"Match found: {person_name} with distance {distance}")
-
-                known_face = cv2.imread(img_path)
-
-                for(x,y,w,h) in faces:
-                    face_region= test_img[y:y+h, x:x+w]
-                    temp_face_path = "temp_face.jpg"
-                    cv2.imwrite(temp_face_path, face_region)
-
-                    face_match = DeepFace.verify(img1_path=img_path,img2_path=temp_face_path, enforce_detection=False)
-
-                    if isinstance(face_match, dict) and "verified" in face_match and face_match["verified"]:
-
-                        best_match = (x,y,w,h)
-                        break
-                if best_match:
-                    x,y,w,h = best_match
-                    cv2.rectangle(test_face, (x,y), (x+w, y+h), (0,255,0), 2)
-
-          
-
-                # Resize the images to 160x160  
-                test_face = cv2.resize(test_face, (160, 160))
-                known_face = cv2.resize(known_face, (160, 160))
-
-                combined_faces= np.hstack((test_face, known_face))
-
-                cv2.imshow("Face comparison", combined_faces)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-
-                if distance<best_match_score:
-                    best_match = person_name
-                    best_match_score = distance
-        except Exception as e:
-            print(f"Error processing {filename}: {e}")
-            print(f"Unexpected result format: {result}")
+    return known_faces
+known_faces = load_known_faces()
+print(known_faces)
 
 
-    if best_match:
-        print(f"Best match: {best_match} with distance {best_match_score}")
-    else:
-        print("No match found")"""
+while cap.isOpened():
+    ret,frame = cap.read()
+    if not ret:
+        break
+    frame_rgb = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
+    results = face_detection.process(frame_rgb)
+
+    # rectangles on face
+    if results.detections:
+        for detection in results.detections:
+            bboxC= detection.location_data.relative_bounding_box
+            h,w,_ = frame.shape
+            x,y,w,h = int(bboxC.xmin*w), int(bboxC.ymin*h), int(bboxC.width*w), int(bboxC.height*h)
+            face = frame[y:y+h, x:x+w]
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+
+            if face.shape[0]>0 and face.shape[1]>0:
+                embedding = get_embedding(face)
+
+                best_match="Unknown"
+                best_score=0.6
+                for name, know_embedding in known_faces.items():
+                    similarity= cosine_similarity(embedding, know_embedding)
+
+                    if similarity>best_score:
+                         best_score= similarity
+                         best_match= name
+
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+                cv2.putText(frame, best_match, (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                cv2.imshow('Face Recognition', frame)
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
